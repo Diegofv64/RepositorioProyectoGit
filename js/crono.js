c61274d8fec7a13f059ba960b8956d3360e703e0
Nizarr18 <131270747+Nizarr18@users.noreply.github.com>
2023-05-09 15:24:09 +0000

    Eliminacion de cronometro y eliminacion de imagen.

--- js/crono.js
+++
@@ -1,9 +0,0 @@
-let time = 0;
-let timerDisplay = document.getElementById('timerDisplay');
-
-function updateTime() {
-  time++;
-  timerDisplay.textContent = time;
-}
-
-setInterval(updateTime, 1000);
